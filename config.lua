135dbb68292ec71d89f94fb811d35b2c419d6f05
Leah <mail@leahuk.com>
2022-01-24 09:10:31 +0000

    Removed version checking on intervals

--- config.lua
+++ config.lua
@@ -2,7 +2,6 @@ Config = {}
 
 Config.NotifyType = "mythic_notify" -- Options = t-notify, esx, mythic_notify, okokNotify
 Config.LoadingType = "mythic" -- Options = mythic, pogress, none
-Config.VersionChecks = true -- When true bixbi addons will check for latest versions once every 2 hours.
 Config.OxInventory = true -- When true, ox inventory related exports will work.
 
 Config.IllegalTaskBlacklist = {
